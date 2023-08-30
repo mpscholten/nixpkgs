df6302a4054ef8607dd03e561575616c4904c76e
Marc Scholten <marc@digitallyinduced.com>
2023-08-29 17:39:53 -0700

    fixed version

--- pkgs/development/compilers/ghc/head.nix
+++ pkgs/development/compilers/ghc/head.nix
@@ -1,5 +1,5 @@
 import ./common-hadrian.nix {
-  version = "9.6.2";
+  version = "9.7.0";
   rev = "1b0f961ecdebd82c172def5b4b17acf5b0a5f13d";
   sha256 = "sha256-ThyoUENfyIQGosYfu5l+URozcu833atgYNFzgbaARfI=";
   url = "https://gitlab.haskell.org/lexi.lambda/ghc.git";
