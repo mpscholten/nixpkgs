05c1f82e2434337f1ecb2cc9f035fc37cd1832b2
Marc Scholten <marc@digitallyinduced.com>
2023-08-29 17:20:56 -0700

    Use GHC patch

--- pkgs/development/compilers/ghc/head.nix
+++ pkgs/development/compilers/ghc/head.nix
@@ -1,5 +1,6 @@
 import ./common-hadrian.nix {
-  version = "9.7.20230527";
-  rev = "69fdbece5f6ca0a718bb9f1fef7b0ab57cf6b664";
-  sha256 = "13rf1d27wdich0kmbds55by9vj3wz0v9clba9p8qpwz7x7wpcjz2";
+  version = "9.6.2";
+  rev = "1b0f961ecdebd82c172def5b4b17acf5b0a5f13d";
+  sha256 = "13rf1d27wdich0kmbds55by9vj3wz0v9clba9p8qpwz7x7wpcjz3";
+  url = "https://gitlab.haskell.org/lexi.lambda/ghc.git";
 }
