80176a7fda8e96560fb8fb49a6a4400e56dc5e73
Marc Scholten <marc@digitallyinduced.com>
2023-08-29 17:32:59 -0700

    Changed hash

--- pkgs/development/compilers/ghc/head.nix
+++ pkgs/development/compilers/ghc/head.nix
@@ -1,6 +1,6 @@
 import ./common-hadrian.nix {
   version = "9.6.2";
   rev = "1b0f961ecdebd82c172def5b4b17acf5b0a5f13d";
-  sha256 = "13rf1d27wdich0kmbds55by9vj3wz0v9clba9p8qpwz7x7wpcjz3";
+  sha256 = "sha256-ThyoUENfyIQGosYfu5l+URozcu833atgYNFzgbaARfI=";
   url = "https://gitlab.haskell.org/lexi.lambda/ghc.git";
 }
