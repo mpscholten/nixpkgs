d5523a87f28f45c694496c7309583d7e4b1529ed
Marc Scholten <marc@digitallyinduced.com>
2023-09-02 11:31:10 -0700

    fix

--- pkgs/development/haskell-modules/configuration-ghc-9.8.x.nix
+++ pkgs/development/haskell-modules/configuration-ghc-9.8.x.nix
@@ -55,8 +55,6 @@ self: super: {
   unix = null;
   xhtml = null;
   system-cxx-std-lib = null;
-  ghc-toolchain = null;
-  ghc-platform = null;
 
   # https://github.com/tibbe/unordered-containers/issues/214
   unordered-containers = dontCheck super.unordered-containers;
