8d40a9c9fcf2fbf6bef5d2db7f311dd7c7e880a1
Marc Scholten <marc@digitallyinduced.com>
2023-09-02 12:56:58 -0700

    fix

--- pkgs/development/misc/haskell/ghc/ghc-platform.nix
+++ pkgs/development/misc/haskell/ghc/ghc-platform.nix
@@ -8,7 +8,6 @@ mkDerivation {
   version = "v.0.1.0.0";
   src = (fetchFromGitLab {
     domain = "gitlab.haskell.org";
-    group = "ghc";
     owner = "ghc";
     repo = "ghc";
     rev = "3ac423b9645998b178badb6edc7bf3328e7d9e4e";
